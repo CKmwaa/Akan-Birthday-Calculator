7d733412b0cace571e7c21ad122833909fef1f12
Ckmwaa <caleb.mwangi@student.moringaschool.com>
2021-11-14 21:26:03 +0300

    Add days of the week on function myname

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function myname(){
     let DD = parseInt(mydate.value)
     let malenames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
     let femalenames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-   
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 }
