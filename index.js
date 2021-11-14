f71a235de3df3dac117fd90bd9834bc0166b004c
Ckmwaa <caleb.mwangi@student.moringaschool.com>
2021-11-14 23:10:45 +0300

    Add a var general in JS

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ function myname(){
     let femalenames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
     let mydate= document.getElementById("date")
-    let mymonth= document.getElementById("month")
+  
+
     let myyear= document.getElementById("year")
     let mymale= document.getElementById("male")
     let myfemale= document.getElementById("female")
+    let CC = parseInt(myyear.value.slice(0,2))
+    let YY = parseInt(myyear.value.slice(2,4))
+    let MM = parseInt(mymonth.value)
+    let DD = parseInt(mydate.value)
     let week = ( ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7 ).toFixed()
     var general = "";
     if(week == 0){
@@ -15,10 +20,6 @@ function myname(){
     else{
         general = week -1
     }
-    let CC = parseInt(myyear.value.slice(0,2))
-    let YY = parseInt(myyear.value.slice(2,4))
-    let MM = parseInt(mymonth.value)
-    let DD = parseInt(mydate.value)
   if(mymale.checked)
   {
   document.getElementById("culture").innerHTML="Akan name is "+malenames[general]+" born on "+days[general]
